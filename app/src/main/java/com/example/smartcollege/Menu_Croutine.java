b973caa3998829fa8300ded305d4ef90ae3a80e4
Rakibul Islam Rony <rakibulrony39@gmail.com>
2019-05-13 04:33:45 +0600

    updated on 13-05-19

--- app/src/main/java/com/example/smartcollege/Menu_Croutine.java
+++ app/src/main/java/com/example/smartcollege/Menu_Croutine.java
@@ -1,13 +1,31 @@
 package com.example.smartcollege;
 
+import android.content.Intent;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Menu_Croutine extends AppCompatActivity {
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.menu_class_routine);
+        button = (Button)findViewById(R.id.btn_login);
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                slideOpen();
+            }
+
+        });
+    }
+
+    public void slideOpen(){
+        Intent intent = new Intent(this, FlashScreen.class);
+        startActivity(intent);
     }
 }
