9dc304f93470db91628ba8e5ec4d78ea331613d8
Rakibul Islam Rony <rakibulrony39@gmail.com>
2019-05-15 14:38:02 +0600

    updated on 15-04-19

---
+++ app/src/main/java/com/example/smartcollege/LoginPage.java
@@ -0,0 +1,10 @@
+package com.example.smartcollege;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FlashScreen extends AppCompatActivity {
+    
+}
